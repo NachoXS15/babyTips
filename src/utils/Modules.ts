8234105292a51d1dec443c8386a41d094394a0ca
NachoXS-NET <pantojaignacio642@gmail.com>
2025-02-20 11:16:38 -0300

    created bio and updated modules

---
+++ src/utils/Modules.ts
@@ -0,0 +1,30 @@
+const ModulesInfo = [
+    {
+        id: 1,
+        moduleInfo: [
+            "Video 1 - ¡Bienvenidos!",
+            "Video 2 - Alimentación Complementaria",
+            "Video 3 - Resumen del Programa"
+        ]
+    },
+    {
+        id: 2,
+        moduleInfo: [
+            "Video 1 - ¿Cuándo puedo iniciar la Alimentación Complementaria?",
+            "Video 2 - Papel de la Leche Materna y la Leche de Fórmula",
+            "Video 3 - Recomendaciones cuando inicias la AC",
+            "Video 4 - Acciones a aplicar si tu bebé no muestra interés en comer",
+        ]
+    },
+    {
+        id: 3,
+        moduleInfo: [
+            "Video 1 - ¿Cómo se si mi bebé come suficiente?",
+            "Video 2 - Cantidades y Frecuencias",
+            "Video 3 - Atoramiento y Ahogamiento",
+            "Video 3 - Manejo de Alergias",
+        ]
+    },
+]
+
+export default ModulesInfo
