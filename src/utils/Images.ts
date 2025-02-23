0f1a460b8e8c69ef851bb0bc923197c3a4cd5c18
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-02-23 13:13:08 -0300

    fixed bio, created testimonies and fixed visual bugs

---
+++ src/utils/Images.ts
@@ -0,0 +1,18 @@
+const Babies = [
+    "/babies/01.jpeg",
+    "/babies/02.jpeg",
+    "/babies/10.jpeg",
+    "/babies/13.jpeg",
+    "/babies/14.jpeg",
+    "/babies/15.jpeg",
+    "/babies/16.jpeg",
+    "/babies/19.jpeg",
+    "/babies/21.jpeg",
+    "/babies/22.jpeg",
+    "/babies/25.jpeg",
+    "/babies/26.jpeg",
+    "/babies/27.jpeg",
+    "/babies/28.jpeg",
+]
+
+export default Babies
