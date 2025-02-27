e563913a695b49ce571b0c0bcae4a917fd88d7a0
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-02-27 15:02:49 -0300

    created banner

---
+++ src/utils/IndexInfo.ts
@@ -0,0 +1,13 @@
+const Texts = [
+    "Con nuestro método de auto-aprendizaje",
+    "Y nuestro método de auto-regulación",
+    "Tu bebé comerá solo antes de los 12 meses",
+]
+
+const Images = [
+    "/index/bg8.webp",
+    "/index/bg9.webp",
+    "/index/bg10.webp"
+]
+
+export {Texts, Images}
