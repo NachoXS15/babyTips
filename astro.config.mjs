56a52f88392b9b4dd99481df1181cec9704ef08f
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-06-03 17:57:00 -0300

    updating api conversion and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });
--- src/pages/api/meta-conversion.ts
+++ src/pages/api/meta-conversion.ts
@@ -1,32 +1,34 @@
-// import type { APIRoute } from 'astro';
-// const accessToken = import.meta.env.ACCESS_TOKEN;
-// const pixelId = import.meta.env.PIXEL_ID;
+export const prerender = false;
 
-// export const POST: APIRoute = async ({ request }) => {
-//   const body = await request.json();
-//   const event_id = body.event_id;
-//   const event_source_url = body.event_source_url;
-//   const client_user_agent = request.headers.get('user-agent') || '';
+import type { APIRoute } from 'astro';
+const accessToken = import.meta.env.ACCESS_TOKEN;
+const pixelId = import.meta.env.PIXEL_ID;
 
-//   const payload = {
-//     data: [
-//       {
-//         event_name: "PageView",
-//         event_time: Math.floor(Date.now() / 1000),
-//         event_id: event_id,
-//         event_source_url: event_source_url,
-//         client_user_agent: client_user_agent,
-//         action_source: "website"
-//       }
-//     ]
-//   };
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json();
+  const event_id = body.event_id;
+  const event_source_url = body.event_source_url;
+  const client_user_agent = request.headers.get('user-agent') || '';
 
-//   const response = await fetch(`https://graph.facebook.com/v19.0/${pixelId}/events?access_token=${accessToken}`, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(payload)
-//   });
+  const payload = {
+    data: [
+      {
+        event_name: "PageView",
+        event_time: Math.floor(Date.now() / 1000),
+        event_id: event_id,
+        event_source_url: event_source_url,
+        client_user_agent: client_user_agent,
+        action_source: "website"
+      }
+    ]
+  };
 
-//   const result = await response.json();
-//   return new Response(JSON.stringify(result), { status: 200 });
-// };
+  const response = await fetch(`https://graph.facebook.com/v19.0/${pixelId}/events?access_token=${accessToken}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  });
+
+  const result = await response.json();
+  return new Response(JSON.stringify(result), { status: 200 });
+};
