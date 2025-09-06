316983ad88f657cb96f56c9ec9ac5c3143b9effb
NachoXS-NET <pantojaignacio642@gmail.com>
2025-09-05 22:24:37 -0300

    created toast msg

---
+++ src/components/Toast.tsx
@@ -0,0 +1,30 @@
+
+import { Toaster, toast } from "react-hot-toast";
+import { nombresLatinos } from "../utils/Nombres";
+import { useEffect, useState } from "react";
+export default function Toast() {
+    const randomName = () => {
+        let randomName = ""
+        let randomIndex = 0
+        randomIndex = Math.floor(Math.random() * nombresLatinos.length)
+        randomName = nombresLatinos[randomIndex]
+        return randomName
+    }
+    const [selectedName, setSelectedName] = useState<string>(randomName)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            randomName()
+            setSelectedName(randomName);
+            toast.success(`${selectedName} ha comprado nuestra receta 🌟🎉`, {duration: 2500})
+        }, 10000)
+
+        return () => clearInterval(interval)
+    })
+
+    return (
+        <div>
+            <Toaster position="top-right"  />
+        </div>
+    );
+}
